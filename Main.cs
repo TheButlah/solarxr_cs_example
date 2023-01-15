454119a04f9a84a971d1d77f9d0e89d52f709d31
Ryan Butler <thebutlah@gmail.com>
2023-01-14 21:09:21 -0500

    Initial project scaffold

---
+++ Main.cs
@@ -0,0 +1,16 @@
+﻿using Console = System.Console;
+using solarxr_protocol;
+
+// Reference https://github.com/jamescourtney/FlatSharp/tree/main/samples for more info.
+
+namespace example
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            MessageBundle b; // TODO
+            Console.WriteLine("hello");
+        }
+    }
+}
